b756cedcb5b7247e7d0b0238502b8515e40ed77b
cark666 <carlos666.cgm@gmail.com>
2022-02-16 18:50:00 +0100

    elimino services

--- src/app/public/public.service.ts
+++ src/app/public/public.service.ts
@@ -9,25 +9,6 @@ export class PublicService{
 
 constructor(private http:HttpClient) {}
 
-getUsers():Observable<any> {
-return this.http.get('https://jsonplaceholder.typicode.com/users').pipe(
-    tap(console.log),
-    catchError(this.handleError)
-)
-}
-getPosts():Observable<any> {
-    return this.http.get('https://jsonplaceholder.typicode.com/posts').pipe(
-        tap(console.log),
-        catchError(this.handleError)
-    )
-}
-getComments():Observable<any> {
-    return this.http.get('https://jsonplaceholder.typicode.com/comments').pipe(
-        tap(console.log),
-        catchError(this.handleError)
-    )
-}
-
 private handleError(error: Response){
 
     console.log(error)
